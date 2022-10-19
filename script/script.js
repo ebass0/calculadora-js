0c892ef88a21d97b495bc247672a2e04d6f04da8
extevo-b <105250571+extevo-b@users.noreply.github.com>
2022-10-19 08:26:37 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,44 @@
+function numero(elementId) {
+    var outputValueTo = document.getElementById('res');
+  
+    if (outputValueTo.value == '0' || outputValueTo.value == 'Syntax error') {
+      outputValueTo.value = elementId.textContent;
+    } else {
+  
+      outputValueTo.value += elementId.textContent;
+    }
+  }
+
+function operacao(elementId) {
+  var outputValueTo = document.getElementById('res');
+  if (outputValueTo.value == '0' || outputValueTo.value == 'Syntax error') {
+    outputValueTo.value = '0';
+  } else {
+    outputValueTo.value += elementId.textContent;
+    document.getElementById('decimal').disabled = false;
+  }
+}
+
+function cleartxt() {
+  document.getElementById('res').value = '0';
+  document.getElementById('decimal').disabled = false;
+}
+
+function decimal(elementId, status) {
+  var outputValueTo = document.getElementById('res');
+  outputValueTo.value += elementId.textContent;
+  document.getElementById('decimal').disabled = status;
+}
+
+function calcular() {
+
+  try {
+    var restxt = document.getElementById('res');
+    if (restxt.value != '') {
+      var calcularResultado = eval(restxt.value);
+      restxt.value = calcularResultado;
+    }
+  } catch (err) {
+    restxt.value = "Syntax error";
+  }
+}
